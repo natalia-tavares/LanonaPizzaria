bc50689e35ba6b9e03387974d06e98c9c4a3b856
natalia-tavares <the-nat@hotmail.com>
2021-07-22 18:56:11 -0300

    first comit

---
+++ js/pizzas.js
@@ -0,0 +1,165 @@
+let pizzaJson = [
+  {
+    id: 1,
+    name: "Moda da Casa",
+    img: "images/pizza.png",
+    price: 20.19,
+    sizes: ["100g", "530g", "860g"],
+    description:
+      "Calabresa, Ervilha, Frango Desfiado, Milho, Mussarela, Orégano e Ovo",
+  },
+
+  {
+    id: 2,
+    name: "Calabresa",
+    img: "images/pizza2.png",
+    price: 40.0,
+    sizes: ["320g", "530g", "860g"],
+    description:
+      "Calabresa fatiada e cebola",
+  },
+  {
+    id: 3,
+    name: "Pepperoni",
+    img: "images/pizza3.png",
+    price: 17.45,
+    sizes: ["320g", "530g", "860g"],
+    description:
+      "Pepperoni e mussarela",
+  },
+  {
+    id: 4,
+    name: "Portuguesa",
+    img: "images/pizza4.png",
+    price: 19.77,
+    sizes: ["320g", "530g", "860g"],
+    description:
+      "Presunto, ervilha, milho, ovo, mussarela e cebola",
+  },
+  {
+    id: 5,
+    name: "Atum",
+    img: "images/pizza5.png",
+    price: 21.43,
+    sizes: ["320g", "530g", "860g"],
+    description:
+      "Atum ralado e cebola",
+  },
+  {
+    id: 6,
+    name: "Frango com Catupiry ",
+    img: "images/pizza6.png",
+    price: 18.55,
+    sizes: ["320g", "530g", "860g"],
+    description:
+      "Frango, Bacon e Catupiry",
+  },
+  {
+    id: 7,
+    name: "4 queijos",
+    img: "images/pizza7.png",
+    price: 22.36,
+    sizes: ["320g", "530g", "860g"],
+    description:
+      "Mussarela, Catupiry, provolone e parmesão",
+  },
+  {
+    id: 8,
+    name: "Alho",
+    img: "images/pizza8.png",
+    price: 22.36,
+    sizes: ["320g", "530g", "860g"],
+    description:
+      "Mussarela e alho frito",
+  },
+  {
+    id: 9,
+    name: "Mussarela",
+    img: "images/pizza9.png",
+    price: 22.36,
+    sizes: ["320g", "530g", "860g"],
+    description:
+      "Mussarela, rodelas de tomates e orégano",
+  },
+  {
+    id: 10,
+    name: "Milho",
+    img: "images/pizza10.png",
+    price: 22.36,
+    sizes: ["320g", "530g", "860g"],
+    description:
+      "Milho com Catupiry",
+  },
+  {
+    id: 11,
+    name: "Tomate Seco",
+    img: "images/pizza11.png",
+    price: 22.36,
+    sizes: ["320g", "530g", "860g"],
+    description:
+      "Mussarela e tomate seco ",
+  },
+  {
+    id: 12,
+    name: "Margarita",
+    img: "images/pizza12.png",
+    price: 22.36,
+    sizes: ["320g", "530g", "860g"],
+    description:
+      "Mussarela, manjericão, tomate e parmesão",
+  },
+  {
+    id: 13,
+    name: "Banana",
+    img: "images/pizza13.png",
+    price: 22.36,
+    sizes: ["320g", "530g", "860g"],
+    description:
+      "Banana com chocolate ao leite",
+  },
+  {
+    id: 14,
+    name: "Sensação",
+    img: "images/pizza14.png",
+    price: 22.36,
+    sizes: ["320g", "530g", "860g"],
+    description:
+      "Morangos com chocolate ao leite ",
+  },
+  {
+    id: 15,
+    name: "Chocolate Colorido",
+    img: "images/pizza15.png",
+    price: 22.36,
+    sizes: ["320g", "530g", "860g"],
+    description:
+      "Chocolate ao leite com M&M's",
+  },
+  {
+    id: 16,
+    name: "Chocolate",
+    img: "images/pizza16.png",
+    price: 22.36,
+    sizes: ["320g", "530g", "860g"],
+    description:
+      "Chocolate ao leite com granulado",
+  },
+  {
+    id: 17,
+    name: "Meio a Meio",
+    img: "images/pizza17.png",
+    price: 22.36,
+    sizes: ["320g", "530g", "860g"],
+    description:
+      "Chocolate ao leite e chocolate branco",
+  },
+  {
+    id: 18,
+    name: "Romeu e Julieta",
+    img: "images/pizza18.png",
+    price: 22.36,
+    sizes: ["320g", "530g", "860g"],
+    description:
+      "Goiabada e Mussarela",
+  },
+];
---
+++ js/script.js
@@ -0,0 +1,307 @@
+/* --- Geral --- */
+//Variables
+const darkMode = document.querySelector('#darkModeBtn');
+let pizzaModal = document.querySelector('.pizzaInfoModal');
+let pizzaModalContent = pizzaModal.querySelector('.modalContent');
+let key;
+
+let pizzaAmount = 1; //Quantidade de pizzas do modal.
+let cart = []; //Carrinho de compras.
+let modalKey, pizzaPrice, pizzaPriceFloat;
+
+let subtotal = 0;
+let discount = 0;
+let total = 0;
+let cartModal = document.querySelector(".cart");
+let cartVisibility = "hidden";
+
+//Functions
+function closePizzaInfoModal() {
+    pizzaModal.style.opacity = 0;
+
+    setTimeout(() => {
+        pizzaModal.style.display = 'none'
+    }, 300)
+}
+
+
+function closeCartModal() {
+    cartModal.style.opacity = 0;
+
+    setTimeout(() => {
+        cartModal.style.display = 'none'
+    }, 300)
+}
+
+/* --- Dark Mode ---*/
+darkMode.addEventListener('click', () => {
+    document.documentElement.classList.toggle('dark-mode');
+})
+
+/* --- Close Modal Buttons --- */
+document.querySelectorAll('.closeProduct, .mobileCloseModal').forEach((element) => {
+    element.addEventListener("click", () => { closePizzaInfoModal() }); 
+});
+
+/* --- Select pizza size --- */
+document.querySelectorAll('.pizzaSize').forEach((element) => {
+    element.addEventListener("click", () => {
+        document.querySelector('.pizzaSize.selected').classList.remove('selected');
+        element.classList.add('selected');
+
+        /* Set price */
+        pizzaPriceFloat = pizzaJson[key].price
+        let keyValue = element.getAttribute('data-key');
+
+        switch(Number(keyValue)) {
+            case 0:
+                pizzaPriceFloat *= .8
+                break;
+            case 1:
+                pizzaPriceFloat *= .9
+                break
+            default: 
+                console.log("Erro ao calcular o preço. Key = " +  key);
+                break; 
+        }
+
+        pizzaPrice = pizzaPriceFloat.toFixed(2)
+        pizzaModalContent.querySelector('.price').innerHTML = `R$ ${pizzaPrice}`;
+
+        pizzaAmount = 1;
+        document.querySelector('.qntBtn h3').innerHTML = pizzaAmount
+    })
+})
+
+/* --- Change pizza quantity --- */
+document.querySelector(".lessQnt").addEventListener("click", () => {//To decrease pizza amount
+    if (pizzaAmount > 1) {
+        pizzaAmount--
+        let pizzaPriceAct = (pizzaPriceFloat * pizzaAmount).toFixed(2);
+
+        pizzaModalContent.querySelector('.price').innerHTML = `R$ ${pizzaPriceAct}`;
+        document.querySelector('.qntBtn h3').innerHTML = pizzaAmount 
+    } 
+})
+
+document.querySelector(".moreQnt").addEventListener("click", () => {//To increase pizza amount.
+    pizzaAmount++
+    let pizzaPriceAct = (pizzaPrice * pizzaAmount).toFixed(2);
+        
+    pizzaModalContent.querySelector('.price').innerHTML = `R$ ${pizzaPriceAct}`;
+    document.querySelector('.qntBtn h3').innerHTML = pizzaAmount
+})
+
+/* --- Cart --- */
+document.querySelector('.cartIcon').addEventListener("click", () => { //cart eventListener to open modal.
+    if (cartVisibility == "visible") {
+        cartVisibility = "hidden";
+        closeCartModal()
+    } else {
+        cartModal.style.display = "flex";     
+        cartModal.style.visibility = "visible";
+    
+        cartModal.style.opacity = 0;
+        setTimeout(() => {
+            cartModal.style.opacity = 1;
+        }, 30)
+        cartVisibility = "visible";
+    }
+
+})
+
+/* --- Main System --- */
+document.querySelector(".addProduct").addEventListener("click", () => { //Add product to cart
+    //modalKey = Qual pizza
+    //pizzaSize = Tamanho da pizza
+    //pizzaAmount = Quantidade de pizzas
+    let pizzaSize = parseInt(document.querySelector(".pizzaSize.selected").getAttribute('data-key'));
+    let identifier = pizzaJson[modalKey].id + '@' + pizzaSize;
+    let key = cart.findIndex(item => item.identifier == identifier);
+
+    if(key > -1) {
+        cart[key].amount += pizzaAmount
+    } else {
+        cart.push({
+            identifier: identifier,
+            id: pizzaJson[modalKey].id,
+            size: pizzaSize,
+            amount: pizzaAmount
+        })
+    }
+
+    console.log(cart)
+    document.querySelector(".cartIcon span").innerHTML = cart.length;
+    document.querySelector(".cartIcon span").style.display = "flex"
+
+    updateCar(); 
+    closePizzaInfoModal();
+})
+
+pizzaJson.map((item, index) => { //Fill content div with pizzas info & open modal system.
+    let pizzaItem = document.querySelector('.models .pizzaItem').cloneNode(true);
+
+    pizzaItem.setAttribute('data-key', index);
+    pizzaItem.querySelector('.pizzaPrice').innerHTML = `R$ ${item.price.toFixed(2)}`;
+    pizzaItem.querySelector('.pizzaImg img').src = item.img;
+    pizzaItem.querySelector('.pizzaName').innerHTML = item.name;
+    pizzaItem.querySelector('.pizzaDescription').innerHTML = item.description;
+
+    pizzaItem.querySelector('a').addEventListener("click", e => {
+        e.preventDefault();
+        closeCartModal();
+
+        key = e.target.closest('.pizzaItem').getAttribute('data-key');
+        pizzaPrice = pizzaJson[key].price.toFixed(2);
+
+        pizzaModalContent.querySelector('.pizzaDescription').innerHTML = pizzaJson[key].description;
+        pizzaModalContent.querySelector('.pizzaName').innerHTML = pizzaJson[key].name;
+        pizzaModalContent.querySelector('.price').innerHTML = `R$ ${pizzaPrice}`;
+        pizzaModalContent.querySelector('img').src = pizzaJson[key].img;
+        pizzaModalContent.querySelector('.priceArea .qntBtn h3').innerHTML = pizzaAmount;
+
+        pizzaModalContent.querySelectorAll('.pizzaSize').forEach((size, sizeIndex) => {
+            size.classList.remove('selected');
+            if (sizeIndex == 2) size.classList.add('selected');
+            size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex];
+        })
+
+        //open modal animation
+        pizzaModal.style.display = "flex";     
+        pizzaModal.style.visibility = "visible";
+
+        pizzaModal.style.opacity = 0;
+        setTimeout(() => {
+            pizzaModal.style.opacity = 1;
+        }, 30)
+        
+        pizzaAmount = 1;
+        modalKey = key;
+    });
+
+    document.querySelector('main .content').append(pizzaItem);
+});
+
+function updateCar() { //Update cart function
+/*     document.querySelector('') - Do span do header do mobile*/
+
+    if (cart.length > 0) {
+        document.querySelector('.cart .container').innerHTML = '';
+
+        for(let i in cart) {
+            let pizzaItem = pizzaJson.find(item => item.id == cart[i].id);
+            let cartItem = document.querySelector('.models .cartItem').cloneNode(true);
+            let pizzaSizeName;
+
+            subtotal = Math.round(pizzaItem.price * cart[i].amount);
+
+            switch(cart[i].size) {
+                case 0:
+                    pizzaSizeName = "Pequena";
+                    console.log()
+                    break;
+                case 1:
+                    pizzaSizeName = "Média";
+                    break;
+                case 2:
+                    pizzaSizeName = "Grande";
+                    break;
+                default:
+                    pizzaSizeName = "Erro"
+                    console.log("Erro #001")
+            }
+
+            cartItem.querySelector('img').src = pizzaItem.img;
+            cartItem.querySelector('.cartPizzaName').innerHTML = pizzaItem.name;
+            cartItem.querySelector('.pizzaSize').innerHTML = " - " + pizzaSizeName;
+            cartItem.querySelector('.cartQuantityArea .showQuantity').innerHTML = cart[i].amount;
+            cartItem.querySelector('.cartQuantityArea .removeQuantity').addEventListener("click", () => {
+                
+                if(cart[i].amount > 1) {
+                    cart[i].amount--;
+                } else {
+                    console.log(cart);
+                    cart.splice(i, 1);
+                    console.log(cart);
+                }
+                updateCar()
+            })
+
+            cartItem.querySelector('.cartQuantityArea .addQuantity').addEventListener("click", () => {
+                cart[i].amount++;
+                updateCar();
+            })
+
+            document.querySelector('.cart .container').append(cartItem);    
+        }
+
+        discount = Math.round(subtotal * 0.1);
+        total = subtotal - discount;
+
+        document.querySelector('.cart .finalData .text .subTotal').innerHTML = `Subtotal: R$ ${subtotal.toFixed(2)}`;
+        document.querySelector('.cart .finalData .text .discount').innerHTML = `Desconto: R$ ${discount.toFixed(2)}`
+        document.querySelector('.cart .finalData .text .totalText').innerHTML = `Total: R$ ${total.toFixed(2)}`
+
+    }
+}
+
+
+    
+const finaliza = document.querySelector('.botaofinal');
+finaliza.addEventListener('click', () => iniciaDados('modal-container-dados'));
+
+
+function iniciaDados(modalID){
+    const modalDI = document.getElementById(modalID);
+    modalDI.classList.add('.mostrar');
+
+}
+
+
+
+// FORM //
+
+document.getElementById('botaoEnviar')
+
+var nome = document.querySelector('.nome')
+var email = document.querySelector('.email')
+var telefone = document.querySelector('.phone')
+var estado = document.querySelector('.estado')
+var cep = document.querySelector('.cep')
+var endereco = document.querySelector('.endereco')
+var pagamento = document.querySelector('.pag')
+var msgError = document.querySelector('#msgError')
+var msgSuccess = document.querySelector('#msgSuccess')
+
+
+function finalizarcomprar(){
+  if (nome.value != '' && email.value != '') {
+
+    let validaFormulario = JSON.parse(localStorage.getItem('validaFormulario') || '[]')
+
+    validaFormulario.push( 
+      {
+         nome: nome.value, 
+         email: email.value,
+         telefone: telefone.value,
+         estado: estado.value,
+         cep: cep.value,
+         Endereço: endereco.value,
+         Pagamento: pagamento.value
+      }
+    );
+
+    localStorage.setItem('validaFormulario', JSON.stringify(validaFormulario));
+
+    msgSuccess.setAttribute('style', 'display: block')
+    msgSuccess.innerHTML = '<strong>Prontinho! Seu Pedido foi finalizado com sucesso! </strong>'
+    msgError.setAttribute('style', 'display: none')
+    msgError.innerHTML = ''
+ } else {
+  msgError.setAttribute('style', 'display: block')
+  msgError.innerHTML = '<strong>Ops! Algo saiu errado. Precisamos de todos os dados Completos! </strong>'
+  msgSuccess.innerHTML = ''
+  msgSuccess.setAttribute('style', 'display: none')
+   }
+ }
+
